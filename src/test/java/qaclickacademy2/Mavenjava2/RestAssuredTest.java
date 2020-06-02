0b454a7dccb8adfb51f52639f4860792973ac770
Aishwarya <aishwaryaprabhu30@gmail.com>
2020-06-02 00:10:17 -0500

    Asian time

--- src/test/java/qaclickacademy2/Mavenjava2/RestAssuredTest.java
+++ src/test/java/qaclickacademy2/Mavenjava2/RestAssuredTest.java
@@ -9,6 +9,8 @@ public class RestAssuredTest {
 		System.out.println("PostJira1");
 		System.out.println("PostJira2");
 		System.out.println("PostJira3");
+		System.out.println("PostJira4");
+		System.out.println("PostJira5");
 		
 	}
 	
@@ -16,6 +18,8 @@ public class RestAssuredTest {
 	public void deleteTwitter()
 	{
 		System.out.println("deleteTwitter");
+		System.out.println("deleteTwitter2");
+		System.out.println("deleteTwitter3");
 	}
 
 }
